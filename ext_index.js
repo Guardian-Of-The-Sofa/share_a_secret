6e94e59a0ef0d5ad41a5679436aff1c7447c5ebb
Jens Pausewang <jens@hauptsache.net>
2019-08-19 11:11:54 +0200

    Remove unnecessary file.

--- ext_index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-function requireAll(require) {
-    require.keys().forEach(require);
-}
-
-requireAll(require.context('./Resources/Private/', true, /^[^_]+\.scss$/));
-$(() => requireAll(require.context('./Resources/Private/', true, /^[^_]+\.js$/)));
