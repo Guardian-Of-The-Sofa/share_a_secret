ae4da91310eb98b177600228c4b1bad532dbe8ad
Jens Pausewang <jens@hauptsache.net>
2019-08-19 12:56:36 +0200

    Settle some to-dos.
    Version 2.0.0.

--- Classes/Domain/Model/Secret.php
+++ Classes/Domain/Model/Secret.php
@@ -29,8 +29,8 @@ class Secret extends AbstractEntity
      * @param string $message , A plaintext message.
      * @param string $plainPassword , the plaintext password to encrypt the message.
      * @throws EnvironmentIsBrokenException
+     * TODO: Refactor machen, dann nächstes Todo.
      * TODO: linkHash im Konstruktor hinzufügen.
-     * TODO: Setter löschen.
      */
     public function __construct(string $message, $plainPassword)
     {
@@ -43,14 +43,6 @@ class Secret extends AbstractEntity
     public function getMessage(): string
     {
         return $this->message;
-    }
-
-    /**
-     * @param string $message
-     */
-    public function setMessage(string $message): void
-    {
-        $this->message = $message;
     }
 
     /**
@@ -109,9 +101,8 @@ class Secret extends AbstractEntity
      * @param string $plainPassword
      * @return string
      * @throws EnvironmentIsBrokenException
-     * TODO: make private
      */
-    public function encryptMessage(string $message, string $plainPassword)
+    private function encryptMessage(string $message, string $plainPassword)
     {
         $encryptedMessage = Crypto::encryptWithPassword($message, $plainPassword);
 
