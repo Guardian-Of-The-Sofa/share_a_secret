7fcb7a949901292549f1023ec71a902a5c9dcf1c
Jens Pausewang <jens@hauptsache.net>
2019-08-20 16:18:40 +0200

    Remove outsourced methods.

--- Classes/Domain/Model/Secret.php
+++ Classes/Domain/Model/Secret.php
@@ -54,36 +54,6 @@ class Secret extends AbstractEntity
     }
 
     /**
-     * @param string $indexHash
-     */
-    public function setIndexHash(string $indexHash): void
-    {
-        $this->indexHash = $indexHash;
-    }
-
-    /**
-     * @return string
-     * @throws Exception
-     */
-    public static function generateLinkHash(): string
-    {
-        $string = strval((new DateTime())->getTimestamp() * 1.0 / random_int(1, PHP_INT_MAX));
-
-        return hash('sha512', $string);
-    }
-
-    /**
-     * @param string $plainPassword
-     * @return bool
-     * @throws EnvironmentIsBrokenException
-     * @throws WrongKeyOrModifiedCiphertextException
-     */
-    public function validatePassword(string $plainPassword): bool
-    {
-        return Crypto::decryptWithPassword($this->message, $plainPassword);
-    }
-
-    /**
      * @param string $message
      * @param string $plainPassword
      * @return string
@@ -94,18 +64,5 @@ class Secret extends AbstractEntity
         $encryptedMessage = Crypto::encryptWithPassword($message, $plainPassword);
 
         return $encryptedMessage;
-    }
-
-    /**
-     * @param string $password
-     * @return string
-     * @throws EnvironmentIsBrokenException
-     * @throws WrongKeyOrModifiedCiphertextException
-     */
-    public function getDecryptedMessage(string $password)
-    {
-        $decryptedMessage = Crypto::decryptWithPassword($this->message, $password);
-
-        return $decryptedMessage;
     }
 }
