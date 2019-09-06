e52e1cc26c01ed6ce16134db76ea7190804502ac
Jens Pausewang <jens@hauptsache.net>
2019-09-06 16:21:21 +0200

    Add statistic class

---
+++ Classes/Domain/Model/Statistic.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Hn\HnShareSecret\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+class Statistic extends AbstractEntity
+{
+    //TODO: woher weiß extbase, dass hier die id rein soll?
+    protected $secret;
+    protected $created;
+    protected $read;
+    protected $deleted;
+
+    public function __construct(int $read = null, int $deleted = null)
+    {
+        $this->read = $read;
+        $this->deleted = $deleted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+
+    /**
+     * @param mixed $secret
+     */
+    public function setSecret($secret): void
+    {
+        $this->secret = $secret;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param mixed $created
+     */
+    public function setCreated($created): void
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRead(): int
+    {
+        return $this->read;
+    }
+
+    /**
+     * @param int $read
+     */
+    public function setRead(int $read): void
+    {
+        $this->read = $read;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDeleted(): int
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * @param int $deleted
+     */
+    public function setDeleted(int $deleted): void
+    {
+        $this->deleted = $deleted;
+    }
+
+}
