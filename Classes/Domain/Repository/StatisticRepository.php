b70c8db78e3f0ca2753d5167b49c87074a015356
Jens Pausewang <jens@hauptsache.net>
2019-09-06 18:04:31 +0200

    Add method

--- Classes/Domain/Repository/StatisticRepository.php
+++ Classes/Domain/Repository/StatisticRepository.php
@@ -2,6 +2,8 @@
 
 namespace Hn\HnShareSecret\Domain\Repository;
 
+use Hn\HnShareSecret\Domain\Model\Secret;
+use Hn\HnShareSecret\Domain\Model\Statistic;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 class StatisticRepository extends Repository
@@ -9,5 +11,16 @@ class StatisticRepository extends Repository
     public function save()
     {
         $this->persistenceManager->persistAll();
+    }
+
+    /**
+     * @param Secret $secret
+     * @return Statistic
+     */
+    public function findBySecret(Secret $secret): ?Statistic
+    {
+        $query = $this->createQuery();
+        $query->matching($query->equals('secret', $secret));
+        return $query->execute()->getFirst() ?: null;
     }
 }
