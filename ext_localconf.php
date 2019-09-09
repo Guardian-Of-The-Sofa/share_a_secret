b15b026cb7bedeb0292641c8fbd75e93b0e16878
Jens Pausewang <jens@hauptsache.net>
2019-09-09 16:51:11 +0200

    Add configuration for scheduler extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,3 +14,10 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 //$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_hnsharesecret_secret[action]';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_hnsharesecret_secret[linkHash]';
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables'] = array(
+    'tx_hnsharesecret_domain_model_secret' => array(
+        'dateField' => 'crdate',
+        'expirePeriod' => '15'
+    ),
+);
