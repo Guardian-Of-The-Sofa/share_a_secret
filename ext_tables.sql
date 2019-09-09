54e52691598b42fa3d86bea7000210cb671f3b6d
Jens Pausewang <jens@hauptsache.net>
2019-09-09 13:53:28 +0200

    Create table for event log

--- ext_tables.sql
+++ ext_tables.sql
@@ -24,3 +24,17 @@ CREATE TABLE tx_hnsharesecret_domain_model_statistic
     KEY parent (pid)
 );
 
+CREATE TABLE tx_hnsharesecret_domain_model_eventlog
+(
+    uid     int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid     int(11)          DEFAULT '0' NOT NULL,
+
+    secret  int(11) unsigned,
+    date    int(11) unsigned             NOT NULL,
+    event   int(11) unsigned NOT NULL,
+    message text,
+
+    PRIMARY KEY (uid),
+    KEY parent (pid)
+);
+
