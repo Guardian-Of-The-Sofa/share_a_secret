7668d218fef06446b12a73d752484839429b4f91
Jens Pausewang <jens@hauptsache.net>
2019-09-09 09:51:30 +0200

    Import Exceptions

--- Classes/Domain/Repository/StatisticRepository.php
+++ Classes/Domain/Repository/StatisticRepository.php
@@ -4,6 +4,8 @@ namespace Hn\HnShareSecret\Domain\Repository;
 
 use Hn\HnShareSecret\Domain\Model\Secret;
 use Hn\HnShareSecret\Domain\Model\Statistic;
+use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
+use TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 class StatisticRepository extends Repository
