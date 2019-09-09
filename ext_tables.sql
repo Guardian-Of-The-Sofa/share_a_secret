58cfaa6b2d1d5c1df17d58c74f7e13a094fd2b27
Jens Pausewang <jens@hauptsache.net>
2019-09-09 16:50:17 +0200

    Add crdate column to secret table

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,7 @@ CREATE TABLE tx_hnsharesecret_domain_model_secret
     uid          int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
     pid          int(11)          DEFAULT '0' NOT NULL,
 
+    crdate       int(11) unsigned DEFAULT '0' NOT NULL,
     message      text             DEFAULT ''  NOT NULL,
     index_hash   varchar(255)                 NOT NULL,
 
