d2f96ac688ee8b0cff2c81fa5fd75c2b88bea37c
Jens Pausewang <jens@hauptsache.net>
2019-09-10 11:05:38 +0200

    Change version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.8-9.5.99',
+            'typo3' => '8.7.27-9.5.99',
         ],
     ],
     'autoload' => [
