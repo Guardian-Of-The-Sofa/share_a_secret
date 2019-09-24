2059edbafc13b4e2f829c9287c537215dd744a8e
Jens Pausewang <jens@hauptsache.net>
2019-09-24 11:43:20 +0200

    Register backend module

--- Resources/Private/Templates/Secret/InvalidField.js
+++
@@ -1,5 +0,0 @@
-$('.is-invalid').each(function(index, element) {
-    $(element).on('input', function() {
-        $(element).removeClass('is-invalid').siblings('.invalid-feedback').hide();
-    });
-});
--- ext_tables.php
+++ ext_tables.php
@@ -7,4 +7,20 @@
     ''
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Secret js');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Include CSS/JS');
+
+// Module System > Backend Users
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'Hn.ShareASecret',
+    'system',
+    'tx_hnshareasecret',
+    'bottom',
+    [
+        'Log' => 'list',
+    ],
+    [
+        'access' => 'admin',
+        'icon' => 'EXT:belog/Resources/Public/Icons/module-belog.svg',
+        'labels' => 'LLL:EXT:share_a_secret/Resources/Private/Language/lang_mod.xlf',
+    ]
+);
