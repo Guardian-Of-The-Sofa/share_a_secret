279ed6aa216b3f5c36c512d23886b0e133e08447
Jens Pausewang <jens@hauptsache.net>
2019-10-09 11:25:57 +0200

    Add new file

---
+++ Classes/Utility/ArrayModifier.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Hn\ShareASecret\Utility;
+
+class ArrayModifier
+{
+    const ASC = 0;
+    const DESC = 1;
+
+    private static $currentProperty;
+    private static $currentOrder;
+
+    public static function compare($a, $b)
+    {
+        $aValue = self::getPropertyValue($a, self::$currentProperty);
+        $bValue = self::getPropertyValue($b, self::$currentProperty);
+        $compare = ($aValue == $bValue);
+        if($compare) { return 0; }
+
+        $compare = ($aValue < $bValue);
+        if(self::$currentOrder == self::DESC){
+            $compare = (!$compare);
+        }
+        if($compare){
+            return -1;
+        } else{
+            return 1;
+        }
+    }
+
+    public static function getMethodName(string $property)
+    {
+        // Replace first letter by its upper case
+        $property = substr_replace($property, strtoupper(substr($property, 0, 1)),0, 1);
+        return $getMethod = 'get' . $property;
+    }
+
+    public static function getPropertyValue(object $object, string $property)
+    {
+        return $object->{self::getMethodName($property)}();
+    }
+
+    public static function getByNonNullProperty(array $array, string $property) : array
+    {
+        $return = [];
+        foreach ($array as $value){
+            if(self::getPropertyValue($value, $property) !== null){
+                $return[] = $value;
+            }
+        }
+        return $return;
+    }
+
+    public static function sortByProperty(array $array, string $property, int $order = self::ASC)
+    {
+        self::$currentProperty = $property;
+        self::$currentOrder = $order;
+        usort($array, 'self::compare');
+        return $array;
+    }
+
+    public static function groupByPropertyValue(array $array, string $property)
+    {
+        $return = [];
+        foreach ($array as $element){
+            if(!isset($return[self::getPropertyValue($element, $property)])){
+                $return[self::getPropertyValue($element, $property)] = [];
+            }
+            $return[self::getPropertyValue($element, $property)][] = $element;
+        }
+        return $return;
+    }
+
+    public static function mergeArrayOfArrays($array)
+    {
+        $return = [];
+        foreach($array as $arr){
+            $return = array_merge($return, $arr);
+        }
+        return $return;
+    }
+
+    public static function sortByProperties(array $array, array $properties)
+    {
+        if(count($properties) == 0){
+            return $array;
+        }
+        $property = key($properties);
+        $order = array_shift($properties);
+        $array = self::sortByProperty($array, $property, $order);
+        $array = self::groupByPropertyValue($array, $property);
+        foreach ($array as $key => $value){
+            $array[$key] = self::sortByProperties($value, $properties);
+        }
+        $array = self::mergeArrayOfArrays($array);
+        return $array;
+    }
+}
---
+++ Classes/ViewHelpers/TableViewHelper.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Hn\ShareASecret\ViewHelpers;
+
+use Hn\ShareASecret\Utility\ArrayModifier;
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class TableViewHelper extends AbstractViewHelper
+{
+    protected $escapeOutput = false;
+    protected static $columnFormats;
+
+    public function initializeArguments()
+    {
+        $this->registerArgument('elements', 'array', 'The elements to display as rows', true);
+        $this->registerArgument('columns', 'array', 'The columns to display', false);
+        $this->registerArgument('columnNames', 'array', 'An array of column names as key and their label as a value', false);
+        $this->registerArgument('tableHeading', 'string', 'The heading to display', false, '');
+        $this->registerArgument('ordering', 'array', 'The ordering in which to display the elements', false);
+        $this->registerArgument('formats', 'array', 'The Formats in which to display columns. Currently only supporting columns which display dates', false);
+        $this->registerArgument('tableClass', 'string', 'A string containing all classes this table should belong to', false);
+        $this->registerArgument('top', 'int', 'The number of elements to display from the top', false);
+        $this->registerArgument('excludeNull', 'array', 'An array containing column names in which to delete every null value', false);
+        $this->registerArgument('excludeColumns', 'array', 'An array containing column names to exclude from table');
+    }
+
+    public static function formatValue(string $column, $value)
+    {
+        $columnFormat = self::$columnFormats[$column] ?? null;
+        if(!$columnFormat){
+            return $value;
+        }
+        $type = key($columnFormat);
+        $format = $columnFormat[$type];
+
+        if($type == 'date'){
+            $date = new \DateTime();
+            $date->setTimestamp($value);
+            $value = $date->format($format);
+        }
+        return $value;
+    }
+
+    public static function mapOrdering(array $ordering)
+    {
+        $return = [];
+        foreach ($ordering as $key => $value){
+            if($value == 'ASC'){
+                $value = ArrayModifier::ASC;
+            }else{
+                $value = ArrayModifier::DESC;
+            }
+            $return[$key] = $value;
+        }
+        return $return;
+    }
+
+    private static function excludeColumns(array& $columns, array $excludeColumns)
+    {
+        $columns = array_diff($columns, $excludeColumns);
+    }
+
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    )
+    {
+        $return = '';
+        self::$columnFormats = $arguments['formats'] ?? '';
+        $tableHeading = $arguments['tableHeading'] ?? '';
+        $tableClass = $arguments['tableClass'] ?? '';
+        $ordering = $arguments['ordering'] ?? false;
+        $top = $arguments['top'] ?? false;
+        $columns = $arguments['columns'] ?? false;
+        $elements = $arguments['elements'];
+        $excludeNull = $arguments['excludeNull'] ?? false;
+        $excludeColumns = $arguments['excludeColumns'] ?? false;
+        $columnNames = $arguments['columnNames'] ?? false;
+
+        if(!$columns){
+            $element = $elements[0];
+            foreach ($element as $column => $value){
+                $columns[$column] = '';
+            }
+            foreach ($columns as $column => $name){
+                $columns[$column] = $columnNames[$column] ?? $column;
+            }
+        }
+
+        if($excludeColumns){
+            self::excludeColumns($columns, $excludeColumns);
+        }
+
+        // sort array
+        if($ordering){
+            $ordering = self::mapOrdering($ordering);
+            $elements = ArrayModifier::sortByProperties($elements, $ordering);
+        }
+
+        // slice array
+        if($top){
+            $elements = array_slice($elements, 0, $top);
+        }
+
+        // remove null values
+        if($excludeNull){
+            foreach ($excludeNull as $property){
+                $elements = ArrayModifier::getByNonNullProperty($elements, $property);
+            }
+        }
+
+        // create table heading
+        $return .= "<h3> $tableHeading </h3>"
+                .  "<table class=\"table $tableClass\">";
+
+        // create table head
+        $return .= '<tr>';
+        foreach ($columns as $column => $name){
+            $return .= "<th scope=\"col\">$name</th>";
+        }
+        $return .= '</tr>';
+
+        // create table rows
+        foreach ($elements as $element){
+            $return .= '<tr>';
+            $i = 0;
+            foreach ($columns as $column => $name){
+                $value = $element[$column];
+                if($i == 0){
+                    $return .= '<th scope="row">' . self::formatValue($column, $value) . '</th>';
+                    $i++;
+                }else{
+                    $return .= '<td>' . self::formatValue($column, $value) . '</td>';
+                }
+            }
+            /*
+            foreach ($columns as $column => $name){
+                $value = ObjectAccess::getProperty($element, $column);
+                if($i == 0){
+                    $return .= '<th scope="row">' . self::formatValue($column, $value) . '</th>';
+                    $i++;
+                }else{
+                    $value = ObjectAccess::getProperty($element, $column);
+                    $return .= '<td>' . self::formatValue($column, $value) . '</td>';
+                }
+            }
+            */
+            $return .=  '</tr>';
+        }
+        $return .= '</table>';
+        return $return;
+    }
+}
