9d2a828fa931a6982d93fa658af336355c4a852c
Jens Pausewang <jens@hauptsache.net>
2019-10-09 12:07:16 +0200

    Add method getStatistics()

--- Classes/Service/EventLogService.php
+++ Classes/Service/EventLogService.php
@@ -1,10 +1,14 @@
 <?php
 
+
 namespace Hn\ShareASecret\Service;
 
 use Hn\ShareASecret\Domain\Model\EventLog;
 use Hn\ShareASecret\Domain\Model\Secret;
 use Hn\ShareASecret\Domain\Repository\EventLogRepository;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 
 class EventLogService
 {
@@ -49,8 +53,95 @@ class EventLogService
         $this->log(new EventLog(EventLog::NOTFOUND, $secret));
     }
 
-    public function findAllDescending()
+    public function getStatistics()
     {
-        return $this->eventLogRepository->findAllDescending();
+        $return = [
+            'totalStatistic' => [0 => []],
+            'unreadSecrets' => null,
+            'readSecrets' => null,
+            'existingSecrets' => null,
+        ];
+
+        /**
+         * get the count of all created Secrets
+         */
+        /* @var QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('tx_shareasecret_domain_model_eventlog');
+        $statement = $queryBuilder
+            ->count('*')
+            ->from('tx_shareasecret_domain_model_eventlog')
+            ->where(
+                $queryBuilder->expr()->eq(
+                    'event',
+                    $queryBuilder->createNamedParameter(EventLog::CREATE)
+                )
+            )
+            ->execute();
+        $createdSecretsCount = array_shift($statement->fetch());
+        $return['totalStatistic'][0]['createdSecretsCount'] = $createdSecretsCount;
+
+        /**
+         * get all secrets which have been read
+         */
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select(
+                'secret.uid AS secretID',
+                        'secret.crdate AS creationDate',
+                        'eventlog.date AS readDate'
+            )
+            ->from('tx_shareasecret_domain_model_secret', 'secret')
+            ->innerJoin(
+                'secret',
+                'tx_shareasecret_domain_model_eventlog',
+                'eventlog',
+                $queryBuilder->expr()->eq(
+                    'secret.uid',
+                    $queryBuilder->quoteIdentifier('eventlog.secret')
+                )
+            )
+            ->where(
+                $queryBuilder->expr()->eq(
+                    'event',
+                    $queryBuilder->createNamedParameter(EventLog::SUCCESS)
+                )
+            )
+            ->groupBy('secret')
+            ->execute();
+        $readSecrets = $statement->fetchAll();
+        $readSecretIDs = [];
+        foreach ($readSecrets as $value){
+            $readSecretIDs[] = $value['secretID'];
+        }
+        $return['readSecrets'] = $readSecrets;
+        $return['totalStatistic'][0]['readSecrets'] = count($return['readSecrets']);
+
+        /**
+         * get all unread secrets
+         **/
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select('*')
+            ->from('tx_shareasecret_domain_model_secret')
+            ->where(
+                $queryBuilder->expr()->notIn('uid', $readSecretIDs)
+            )
+            ->execute();
+        $unreadSecrets = $statement->fetchAll();
+        $return['totalStatistic'][0]['unreadSecrets'] = count($unreadSecrets);
+        $return['unreadSecrets'] = $unreadSecrets;
+
+        /**
+         * Get all secrets in the database
+         */
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select('*')
+            ->from('tx_shareasecret_domain_model_secret')
+            ->execute();
+        $existingSecrets = $statement->fetchAll();
+        $return['existingSecrets'] = $existingSecrets;
+        return $return;
     }
 }
