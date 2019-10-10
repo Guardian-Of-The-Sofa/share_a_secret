a99643edba4939469a67e4d59e108a3d4e20e683
Jens Pausewang <jens@hauptsache.net>
2019-10-10 15:57:33 +0200

    Add statistic for most recent events
    Code refactoring

--- Classes/Service/EventLogService.php
+++ Classes/Service/EventLogService.php
@@ -17,9 +17,15 @@ class EventLogService
      */
     private $eventLogRepository;
 
+    /**
+     * @var QueryBuilder $preparedQueryBuilder
+     */
+    private $preparedQueryBuilder;
+
     public function __construct(EventLogRepository $eventLogRepository)
     {
         $this->eventLogRepository = $eventLogRepository;
+        $this->initPreparedQueryBuilder();
     }
 
     public function log(EventLog $eventLog)
@@ -53,6 +59,154 @@ class EventLogService
         $this->log(new EventLog(EventLog::NOTFOUND, $secret));
     }
 
+    private function initPreparedQueryBuilder()
+    {
+        $this->preparedQueryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('tx_shareasecret_domain_model_eventlog');
+
+        $this->preparedQueryBuilder
+            ->select(
+                'el.date AS creationDate',
+                'el.secret',
+                'el.event AS creationEvent',
+                'er.date AS logDate',
+                'er.event'
+            )
+            ->from('tx_shareasecret_domain_model_eventlog', 'el')
+            ->innerJoin(
+                'el',
+                'tx_shareasecret_domain_model_eventlog',
+                'er',
+                $this->preparedQueryBuilder->expr()->eq(
+                    'el.secret',
+                    $this->preparedQueryBuilder->quoteIdentifier('er.secret')
+                )
+            )
+            ->where(
+                $this->preparedQueryBuilder->expr()->eq(
+                    'el.event',
+                    $this->preparedQueryBuilder->createNamedParameter(EventLog::CREATE)
+                )
+            );
+    }
+
+    public function getPreparedQueryBuilder()
+    {
+        return $this->preparedQueryBuilder;
+    }
+
+    public function getCreatedSecrets()
+    {
+        /* @var QueryBuilder $queryBuilder */
+        $queryBuilder = clone $this->getPreparedQueryBuilder();
+
+        /**
+         * get all created secrets
+         */
+        $statement = $queryBuilder
+            ->andWhere(
+                $queryBuilder->expr()->eq(
+                    'er.event',
+                    $queryBuilder->createNamedParameter(EventLog::CREATE)
+                )
+            )
+            ->groupBy('secret')
+            ->execute();
+        return $statement->fetchAll();
+    }
+
+    public function getReadSecrets()
+    {
+        $queryBuilder = clone $this->getPreparedQueryBuilder();
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select(
+                'el.date AS creationDate',
+                'el.secret',
+                'el.event AS creationEvent',
+                'er.date AS logDate',
+                'er.event'
+            )
+            ->from('tx_shareasecret_domain_model_eventlog', 'el')
+            ->innerJoin(
+                'el',
+                'tx_shareasecret_domain_model_eventlog',
+                'er',
+                $queryBuilder->expr()->eq(
+                    'el.secret',
+                    $queryBuilder->quoteIdentifier('er.secret')
+                )
+            )
+            ->where(
+                $queryBuilder->expr()->eq(
+                    'el.event',
+                    $queryBuilder->createNamedParameter(EventLog::CREATE)
+                )
+            )
+            ->andWhere(
+                $queryBuilder->expr()->eq(
+                    'er.event',
+                    $queryBuilder->createNamedParameter(EventLog::SUCCESS)
+                )
+            )
+            ->groupBy('secret')
+            ->execute();
+        return $statement->fetchAll();
+    }
+
+    public function getUnreadSecrets(array $readSecretIDs)
+    {
+        $queryBuilder = clone $this->getPreparedQueryBuilder();
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select('*')
+            ->from('tx_shareasecret_domain_model_secret')
+            ->where(
+                $queryBuilder->expr()->notIn('uid', $readSecretIDs)
+            )
+            ->execute();
+        return $statement->fetchAll();
+    }
+
+    public function getExistingSecrets()
+    {
+        $queryBuilder = clone $this->getPreparedQueryBuilder();
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select('*')
+            ->from('tx_shareasecret_domain_model_secret')
+            ->execute();
+        return $statement->fetchAll();
+    }
+
+    public function getDeletedSecrets()
+    {
+        $queryBuilder = clone $this->getPreparedQueryBuilder();
+        $queryBuilder->resetQueryParts();
+        $statement = $queryBuilder
+            ->select('*')
+            ->from('tx_shareasecret_domain_model_eventlog', 'eventlog')
+            ->where(
+                $queryBuilder->expr()->eq('event', $queryBuilder->createNamedParameter(EventLog::DELETE))
+            )
+            ->execute();
+        return $statement->fetchAll();
+    }
+
+    public function getMostRecentEvents(int $n)
+    {
+        $queryBuilder = clone $this->getPreparedQueryBuilder();
+        $queryBuilder->resetQueryParts();
+
+        $statement = $queryBuilder
+            ->select('*')
+            ->from('tx_shareasecret_domain_model_eventlog')
+            ->orderBy('uid', 'DESC')
+            ->setMaxResults($n);
+
+        return $statement->execute()->fetchAll();
+    }
+
     public function getStatistics()
     {
         $return = [
@@ -62,109 +216,38 @@ class EventLogService
             'existingSecrets' => null,
             'createdSecrets' => null,
             'deletedSecrets' => null,
+            'mostRecentEvents' => null,
         ];
 
-        /**
-         * get all created secrets
-         */
-        /* @var QueryBuilder $queryBuilder */
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
-            ->getQueryBuilderForTable('tx_shareasecret_domain_model_eventlog');
-        $statement = $queryBuilder
-            ->select('*')
-            ->from('tx_shareasecret_domain_model_eventlog')
-            ->where(
-                $queryBuilder->expr()->eq(
-                    'event',
-                    $queryBuilder->createNamedParameter(EventLog::CREATE)
-                )
-            )
-            ->execute();
-        $createdSecrets = $statement->fetchAll();
-        $createdSecretsCount = count($createdSecrets);
-        $return['totalStatistic'][0]['createdSecretsCount'] = $createdSecretsCount;
+        $createdSecrets = $this->getCreatedSecrets();
         $return['createdSecrets'] = $createdSecrets;
+        $return['totalStatistic'][0]['createdSecrets'] = count($createdSecrets);
 
-        /**
-         * get all secrets which have been read
-         */
-        $queryBuilder->resetQueryParts();
-        $statement = $queryBuilder
-            ->select(
-                'eventlog.secret AS secretID',
-                        'secret.crdate AS creationDate',
-                        'eventlog.date AS readDate'
-            )
-            ->from('tx_shareasecret_domain_model_secret', 'secret')
-            ->rightJoin(
-                'secret',
-                'tx_shareasecret_domain_model_eventlog',
-                'eventlog',
-                $queryBuilder->expr()->eq(
-                    'secret.uid',
-                    $queryBuilder->quoteIdentifier('eventlog.secret')
-                )
-            )
-            ->where(
-                $queryBuilder->expr()->eq(
-                    'event',
-                    $queryBuilder->createNamedParameter(EventLog::SUCCESS)
-                )
-            )
-            ->groupBy('secret')
-            ->execute();
-        $readSecrets = $statement->fetchAll();
+        $readSecrets = $this->getReadSecrets();
+        $return['readSecrets'] = $readSecrets;
+        $return['totalStatistic'][0]['readSecrets'] = count($readSecrets);
+
         $readSecretIDs = [];
         foreach ($readSecrets as $value){
-            if($value['secretID']){
-                $readSecretIDs[] = $value['secretID'];
+            if($value['secret']){
+                $readSecretIDs[] = $value['secret'];
             }
         }
-        $return['readSecrets'] = $readSecrets;
-        $return['totalStatistic'][0]['readSecrets'] = count($return['readSecrets']);
-
-        /**
-         * get all unread secrets
-         **/
-        $queryBuilder->resetQueryParts();
-        $statement = $queryBuilder
-            ->select('*')
-            ->from('tx_shareasecret_domain_model_secret')
-            ->where(
-                $queryBuilder->expr()->notIn('uid', $readSecretIDs)
-            )
-            ->execute();
-        $unreadSecrets = $statement->fetchAll();
-        $return['totalStatistic'][0]['unreadSecrets'] = count($unreadSecrets);
+        $unreadSecrets = $this->getUnreadSecrets($readSecretIDs);
         $return['unreadSecrets'] = $unreadSecrets;
+        $return['totalStatistic'][0]['unreadSecrets'] = count($unreadSecrets);
 
-        /**
-         * Get all secrets in the database
-         */
-        $queryBuilder->resetQueryParts();
-        $statement = $queryBuilder
-            ->select('*')
-            ->from('tx_shareasecret_domain_model_secret')
-            ->execute();
-        $existingSecrets = $statement->fetchAll();
+        $existingSecrets = $this->getExistingSecrets();
         $return['existingSecrets'] = $existingSecrets;
+        $return['totalStatistic'][0]['existingSecrets'] = count($existingSecrets);
 
-        /**
-         * Get all deleted secrets
-         */
-        $queryBuilder->resetQueryParts();
-        $statement = $queryBuilder
-            ->select('*')
-            ->from('tx_shareasecret_domain_model_eventlog', 'eventlog')
-            ->where(
-                $queryBuilder->expr()->eq('event', $queryBuilder->createNamedParameter(EventLog::DELETE))
-            )
-            ->execute();
-        $deletedSecrets= $statement->fetchAll();
+        $deletedSecrets = $this->getDeletedSecrets();
         $deletedSecretsCount = count($deletedSecrets);
-        $return['totalStatistic'][0]['deletedSecrets'] = $deletedSecretsCount;
         $return['deletedSecrets'] = $deletedSecrets;
+        $return['totalStatistic'][0]['deletedSecrets'] = $deletedSecretsCount;
 
+        $mostRecentEvents = $this->getMostRecentEvents(10);
+        $return['mostRecentEvents'] = $mostRecentEvents;
         return $return;
     }
 }
