04884b9fd7a8f2b5f6fa19af6188547ee034be4c
Jens Pausewang <jens@hauptsache.net>
2019-10-18 13:18:33 +0200

    Add configuration for RequireJS

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,9 @@
 <?php
+
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\DebugUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 defined('TYPO3_MODE') || die('Access denied.');
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Hn.ShareASecret',
@@ -20,3 +25,24 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Sched
         'expirePeriod' => '15'
     ),
 );
+
+if(TYPO3_MODE === 'BE') {
+    /** @var PageRenderer $pageRenderer */
+    $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+    $pageRenderer->addRequireJsConfiguration([
+        'paths' => [
+            'highcharts' => 'https://code.highcharts.com',
+            'highcharts/highstock' => 'https://code.highcharts.com/stock',
+        ],
+        'packages' => [
+            1 => [
+                'name' => 'highcharts',
+                'main' => 'highcharts'
+            ],
+            2 => [
+                'name' => 'highcharts/highstock',
+                'main' => 'highstock'
+            ],
+        ],
+    ]);
+}
