d9ba36d0a1c27059a79fe8a06dda83fb73ccf481
Jens Pausewang <jens@hauptsache.net>
2019-12-13 15:03:05 +0100

    Change name of TypoScript include file names

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
 $extKey = 'share_a_secret';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/CssJs', 'Include CSS/JS');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/Libraries', 'Include CSS/JS libraries');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/CssJs', 'CSS/JS');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/Libraries', 'Optional jQuery and Bootstrap libraries');
