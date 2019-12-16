d6da1ce8aaba47a6aeeab7e71e57eec4e3e07a6f
Jens Pausewang <jens@hauptsache.net>
2019-12-16 10:37:12 +0100

    Add css for Bootstrap v.3 compatibility

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,3 +2,4 @@
 $extKey = 'share_a_secret';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/CssJs', 'CSS/JS');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/Libraries', 'Optional jQuery and Bootstrap libraries');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript/BootstrapV3Compatibility', 'CSS styles for Bootstrap v. 3 compatibility');
