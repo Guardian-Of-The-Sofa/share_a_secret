ee1406dc6fc5a40034df956859b5b752c40047ec
Jens Pausewang <jenspausewang@gmx.de>
2021-08-08 13:04:03 +0200

    Define version contraint properly

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.27<11',
+            'typo3' => '8.7.27-10.4.99',
         ],
     ],
     'autoload' => [
